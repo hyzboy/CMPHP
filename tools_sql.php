412c7c5e05a6bd59f8fc0c00f65951109282db29
hyzboy <devnull@localhost>
2016-08-31 15:17:51 +0400

    tools_sql改为使用SESSION保存DB地址和用户名密码等

--- tools_sql.php
+++ tools_sql.php
@@ -1,31 +1,24 @@
 ﻿<?php
 
-    $global_sql=null;
+    static $global_sql=null;
 
-    function connect_to_sql($server,$username,$password,$db)
+    function get_sql()
     {
         global $global_sql;
 
         if($global_sql)
             return $global_sql;
 
-        $global_sql=new mysqli($server,$username,$password,$db);
+        $global_sql=new mysqli($_SESSION["DB_ADDRESS"],$_SESSION["DB_USERNAME"],$_SESSION["DB_PASSWORD"],$_SESSION["DB_NAME"]);
 
         if($global_sql->connect_error)
         {
-            echo "Connection mysql error: ".$sql->connect_error;
+            echo "Connection Database error: ".$sql->connect_error;
             return null;
         }
 
         $global_sql->query("SET NAMES 'utf8'");
-        $global_sql->query("use ".$db);
-
-        return $global_sql;
-    }
-
-    function get_sql()
-    {
-        global $global_sql;
+        $global_sql->query("USE ".$_SESSION["DB_NAME"]);
 
         return $global_sql;
     }
@@ -36,7 +29,7 @@
 
     	if($sql==null)return null;
 
-    	$sql_result=$sql->query("desc ".$table_name);
+    	$sql_result=$sql->query("DESC ".$table_name);
 
     	if(!$sql_result)
     		return null;
@@ -54,11 +47,11 @@
 
         if($sql==null)return;
 
-        $sql_string="select";
+        $sql_string="SELECT";
 
         if($field_list==null)
         {
-        	$sql_string=$sql_string." * from ".$table_name;
+        	$sql_string=$sql_string." * FROM ".$table_name;
         }
         else
         {
@@ -68,13 +61,13 @@
 	            else
 	                $sql_string=$sql_string.','.$field_list[$i];
 
-        	$sql_string=$sql_string." from ".$table_name;
+        	$sql_string=$sql_string." FROM ".$table_name;
         }
 
         if($where!=null&&strlen($where)>3)
-        	$sql_string=$sql_string." where ".$where;
+        	$sql_string=$sql_string." WHERE ".$where;
 
-        if($count!=0)$sql_string=$sql_string." limit ".$start.",".$count;
+        if($count!=0)$sql_string=$sql_string." LIMIT ".$start.",".$count;
 
         $sql_result=$sql->query($sql_string);
 
@@ -98,7 +91,7 @@
 
         if($sql==null)return;
 
-        $sql_string="select ".$field." from ".$table_name." where ".$where;
+        $sql_string="SELECT ".$field." FROM ".$table_name." WHERE ".$where;
 
         $sql_result=$sql->query($sql_string);
 
@@ -111,19 +104,29 @@
         return $row;
     }
 
-    function sql_insert($table_name,$data_array)
+    /**
+    * 向一个表中插入数据，使用array的kv模式表示字段和数据
+    */
+    function sql_insert()//$table_name,$data_array)
     {
     	$sql=get_sql();
 
-    	if($sql==null)return;
+    	if($sql==null)return null;
 
-    	$sql_string="insert into ".$table_name." SET";
+    	$table_name=func_get_arg(0);
+    	$data_array=func_get_arg(1);
+
+    	if(func_num_args()==3)
+            $resultmode=func_get_arg(2);
+        else
+            $resultmode=MYSQLI_STORE_RESULT;
+
+    	$sql_string="INSERT INTO ".$table_name." SET";
 
     	$count=0;
 
     	foreach($data_array as $field=>$value)
     	{
-
     		if($count==0)
     			$sql_string=$sql_string.' '.$field.'="'.$value.'"';
     		else
@@ -132,7 +135,32 @@
     		++$count;
     	}
 
-    	echo 'SQLString: '.$sql_string;
-    	$sql->query($sql_string);
+//    	echo 'SQLString: '.$sql_string;
+    	return $sql->query($sql_string);
+    }
+
+    /**
+    * 向一个表中插入数据，字段数据来自于post
+    */
+    function sql_insert_by_post()//$table_name,$field_array [,$resultmode=MYSQLI_STORE_RESULT])
+    {
+        $sql=get_sql();
+
+        if($sql==null)return null;
+
+        $data_array=array();
+
+    	$table_name=func_get_arg(0);
+    	$field_array=func_get_arg(1);
+
+    	if(func_num_args()==3)
+            $resultmode=func_get_arg(2);
+        else
+            $resultmode=MYSQLI_STORE_RESULT;
+
+        foreach($field_array as $field)
+            $data_array[$field]=$_POST[$field];
+
+        return sql_insert($table_name,$data_array,$resultmode);
     }
 ?>
