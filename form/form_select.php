4040d9cc94b6853e20760b9e6fdbef959a9b0978
hyzboy <devnull@localhost>
2016-09-01 09:13:31 +0400

    from_select选择框后方提示信息显示风格改变

--- form/form_select.php
+++ form/form_select.php
@@ -7,9 +7,30 @@
     	$selected  =func_get_arg(2);
     	$items     =func_get_arg(3);
 
-        echo '<div class="form-group">
+    	echo '<div class="form-group">
                 <label class="control-label col-sm-2">'.$label.'</label>
                 <div class="col-sm-10">';
+
+//     	echo '<div class="dropdown">
+//                 <button class="btn btn-default dropdown-toggle" type="button" id="'.$label.'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+//                     '.$name.'
+//                     <span class="caret"></span>
+//                 </button>
+//                 <ul class="dropdown-menu" aria-labelledby="'.$label.'">';
+//
+//                 foreach($items as $value=>$text)
+//                 {
+//                     if($selected==$value)
+//                         echo '<option value="'.$value.'" selected="selected">'.$text.'</option>';
+//                     else
+//                         echo '<li><a href="#">'.$text.'</a></li>';
+//                         echo '<option value="'.$value.'">'.$text.'</option>';
+//                 }
+//                     <li><a href="#">Action</a></li>
+//                     <li><a href="#">Another action</a></li>
+//                     <li><a href="#">Something else here</a></li>
+//         echo '</ul>
+//             </div>';
 
         echo '<select name="'.$name.'">';
 
@@ -26,7 +47,7 @@
     	if(func_num_args()==5)
     	{
             $right_label=func_get_arg(4);
-            echo '<label class="control-label">'.$right_label.'</label>';
+            echo '<span class="help-block">'.$right_label.'</span>';
         }
 
         echo '</div></div>';
