cc50b84e95d74a215d1a47653d9892a14943cf48
hyzboy <devnull@localhost>
2017-02-16 10:41:40 +0800

    create_check增加是否选中选项

--- form/form_check.php
+++ form/form_check.php
@@ -1,6 +1,11 @@
 ﻿<?php
 
-    function create_check($name,$value,$text)
+    function create_check($name,$value,$text,$checked)
     {
-        echo '<input type="checkbox" name="'.$name.'" value="'.$value.'">'.$text;
+        echo '<input type="checkbox" name="'.$name.'" value="'.$value.'"';
+
+        if($checked=="true")
+            echo ' checked';
+
+        echo '> '.$text;
     }
