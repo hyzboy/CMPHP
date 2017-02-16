471b7f2c15f92ee6b4d6c5344b7eaf82203be857
hyzboy <devnull@localhost>
2017-02-16 10:40:19 +0800

    增加tools_time.php

---
+++ tools_time.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+    function get_localtime()
+    {
+        date_default_timezone_set('Asia/Shanghai');
+
+        return time();
+    }
+
+    function time_mysql_format($timestamp)
+    {
+        return date("Y-m-d H:i:s",$timestamp);
+    }
+
+    function localtime_mysql_format()
+    {
+        return time_mysql_format(get_localtime());
+    }
