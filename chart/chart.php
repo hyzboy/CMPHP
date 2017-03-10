7bb0ffe1e20035d1d8b34f1b152d1b8d569fc468
hyzboy <hyzboy@gmail.com>
2017-03-10 14:58:31 +0800

    成功第一次输出图表

--- chart/chart.php
+++ chart/chart.php
@@ -1,21 +1,30 @@
 <?php
 
+//     XXX         HP  MP  AP
+//
+//     3000    |        ___/
+//     2000    |    ___/
+//     1000    |___/
+//        0    +---+---+---+---
+//             A   B   C   D
+
     class ChartData
     {
-        public $name;
-        public $cols;
-        public $value;
+        public $name;               //数据名称阵列(参加注释图中的HP/MP/AP)
+        public $type;               //类型
+        public $value;              //数据阵列
+        public $stack=null;         //堆叠属性
+        public $area_style=null;    //区域风格
 
         /**
         * @param n 数据名称
-        * @param cols_data 列数据名称
         * @param cols_value 列数据
+        * @param t 数据类型
         **/
-        public function __construct($n,$cols_data,$cols_value)
+        public function __construct($n,$t,$cols_value)
         {
             $this->name=$n;
-
-            $this->cols=$cols_data;
+            $this->type=$t;
             $this->value=$cols_value;
         }
     };//class ChartData
@@ -27,7 +36,10 @@
 
         private $title=null;
 
-        private $type=null;
+        private $x_type=null;
+        private $col_name=null;
+
+        private $save_as_image=false;
 
         private $data_list=array();
 
@@ -36,15 +48,13 @@
         * @param i id
         * @param w 宽
         * @param h 高
-        * @param t 类型
         **/
-        public function __construct($n,$i,$w,$h,$t)
+        public function __construct($n,$i,$w,$h)
         {
             $this->title=$n;
             $this->id=$i;
             $this->width=$w;
             $this->height=$h;
-            $this->type=$t;
         }
 
         public function set_title($t)
@@ -52,14 +62,124 @@
             $this->title=$t;
         }
 
-        public function set_type($t)
+        public function set_save_as_image($sai)
         {
-            $this->type=$t;
+            $this->save_as_image=$sai;
         }
 
-        public function add_data($data)
+        public function set_cols($xt,$cn)
         {
-            array_push($this->data_list,$data);
+            $this->x_type=$xt;
+            $this->col_name=$cn;
+        }
+
+        public function add_data($name,$type,$data)
+        {
+            $cd=new ChartData($name,$type,$data);
+            array_push($this->data_list,$cd);
+            return $cd;
+        }
+
+        private function echo_legend()  //输出标题头
+        {
+            echo 'legend: { data:[';
+
+            $first=true;
+            foreach($this->data_list as $obj)
+            {
+                if($first==false)
+                    echo ',';
+
+                echo '"'.$obj->name.'"';
+                $first=false;
+            }
+
+            echo ']},';
+        }
+
+        private function echo_toolbox() //输出工具箱
+        {
+            echo 'toolbox:
+                  {
+                    feature:
+                    {
+                        saveAsImage:{}
+                    }
+                  },';
+        }
+
+        private function echo_x()       //输出横向轴配置
+        {
+            echo 'xAxis:[
+                  {';
+
+            if($this->x_type)
+            echo '  type:"'.$this->x_type.'",';
+
+            echo '  data:[';
+
+            $first=true;
+            foreach($this->col_name as $name)
+            {
+                if($first==false)
+                    echo ',';
+                else
+                    $first=false;
+
+                echo '"'.$name.'"';
+            }
+
+            echo ']
+            }],';
+        }
+
+        private function echo_y()
+        {
+            echo 'yAxis:[{type:"value"}],';
+        }
+
+        private function echo_series()
+        {
+            echo 'series: [';
+
+            $first=true;
+
+            foreach($this->data_list as $obj)
+            {
+                if($first==false)
+                    echo ',';
+                else
+                    $first=false;
+
+                echo '{
+                        name:"'.$obj->name.'",
+                        type:"'.$obj->type.'",';
+
+                if($obj->stack)
+                echo '  stack:"'.$obj->stack.'",';
+
+                if($obj->area_style)
+                echo '  areaStyle:{'.$obj->area_style.'},';
+
+                echo '  data:[';
+
+                $value_first=true;
+                foreach($obj->value as $value)
+                {
+                    if($value_first==false)
+                        echo ','.$value;
+                    else
+                    {
+                        echo $value;
+                        $value_first=false;
+                    }
+                }
+
+                echo ']
+                    }';
+            }
+
+            echo ']';
         }
 
         public function draw()
@@ -68,7 +188,7 @@
 
             echo '<script type="text/javascript">
 
-                    var myChart=echarts.init(document.getElementById("'.$id.'"));
+                    var myChart=echarts.init(document.getElementById("'.$this->id.'"));
 
                     var option =
                     {
@@ -78,82 +198,18 @@
             echo 'title:{text:"'.$this->title.'"},
                   tooltip:{},';
 
-            echo 'legend: { data:[';
+            $this->echo_legend();
 
-            $first=true;
-            foreach($data_list as $obj)
-            {
-                if($first==false)
-                    echo ',';
+            if($this->save_as_image)
+                $this->echo_toolbox();
 
-                echo '"'.$obj->name.'"';
-                $first=false;
-            }
+            $this->echo_x();
+            $this->echo_y();
 
-            echo ']},
-                xAxis:
-                {';
+            $this->echo_series();
 
-            $first=true;
-            foreach($data_list as $obj)
-            {
-                if($first==false)
-                    echo ',';
-
-                echo 'data:[';
-
-                $col_first=true;
-                foreach($obj->cols as $cols_name)
-                {
-                    if($col_first==false)
-                        echo ',';
-
-                    echo '"'.$cols_name.'"';
-
-                    $col_first=false;
-                }
-
-                echo ']';
-                $first=false;
-            }
-
-            echo '},
-                yAxis:{},
-
-                series:[';
-
-            $first=true;
-            foreach($data_list as $obj)
-            {
-                if($first==false)
-                    echo ',';
-
-                echo '{';
-
-                echo 'name: "'.$obj->name.'",';
-                echo 'type: "'.$this->type.'",';
-
-                echo 'data:[';
-
-                $col_first=true;
-                foreach($obj->value as $value)
-                {
-                    if($col_first==false)
-                        echo ',';
-
-                    echo $value;
-
-                    $col_first=false;
-                }
-
-                echo '}';
-                $first=false;
-            }
-
-            echo ']
-            };
-
-            myChart.setOption(option);
-            </script>';
+            echo '};
+                myChart.setOption(option);
+                </script>';
         }
     };//class Chart
